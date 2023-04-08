35641b2ccb9d31ff109a343f672bbd22d6caca8b
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2023-04-08 14:25:09 +0900

    added external libraries

--- tests/test_example.cpp
+++ tests/test_example.cpp
@@ -1,7 +1,21 @@
 #include <doctest/doctest.h>
-#include <spdlog/spdlog.h>
 #include "boke/boke.h"
 TEST_CASE("log") {
   spdlog::info("hello {}", "world");
   CHECK_UNARY(true);
+}
+TEST_CASE("sid") {
+  namespace sid = foonathan::string_id;
+  using namespace sid::literals;
+  sid::default_database database;
+  sid::string_id id("Test", database);
+  switch (id.hash_code()) {
+    case "Test"_id:
+      CHECK_UNARY(true);
+      break;
+    case "NoTest"_id:
+    default:
+      CHECK_UNARY(false);
+      break;
+  }
 }
