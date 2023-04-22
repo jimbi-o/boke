8536159620bb8e83da27c23aad20ae8d53ab08a5
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2023-04-22 18:29:05 +0900

    Create/Destroy Window function

--- src/gfx.cpp
+++ src/gfx.cpp
@@ -39,21 +39,38 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
   }
   return ::DefWindowProcW(hWnd, msg, wParam, lParam);
 }
+struct WindowInfo {
+  HWND hwnd{};
+  LPWSTR class_name{};
+  HINSTANCE h_instance{};
+};
+auto CreateWin32Window(const rapidjson::Document& json, boke::AllocatorData* allocator_data) {
+  using namespace boke;
+  auto title = ConvertAsciiCharToWchar(json["title"].GetString(), allocator_data);
+  WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, title, nullptr };
+  ::RegisterClassExW(&wc);
+  HWND hwnd = ::CreateWindowW(wc.lpszClassName, title, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, json["screen_width"].GetInt(), json["screen_height"].GetInt(), nullptr, nullptr, wc.hInstance, nullptr);
+  ::ShowWindow(hwnd, SW_SHOWDEFAULT);
+  ::UpdateWindow(hwnd);
+  return WindowInfo {
+    .hwnd = hwnd,
+    .class_name = title,
+    .h_instance = wc.hInstance,
+  };
+}
+auto DestroyWin32Window(WindowInfo& info, boke::AllocatorData* allocator_data) {
+  using namespace boke;
+  ::DestroyWindow(info.hwnd);
+  ::UnregisterClassW(info.class_name, info.h_instance);
+  Deallocate(info.class_name, allocator_data);
+}
 }
 TEST_CASE("imgui") {
   using namespace boke;
   const uint32_t main_buffer_size_in_bytes = 16 * 1024;
   std::byte main_buffer[main_buffer_size_in_bytes];
-  const char config_path[] = "tests/config.json";
   auto allocator_data = GetAllocatorData(main_buffer, main_buffer_size_in_bytes);
-  auto json = GetJson(config_path, allocator_data);
-  auto title = ConvertAsciiCharToWchar(json["title"].GetString(), allocator_data);
-  WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, title, nullptr };
-  ::RegisterClassExW(&wc);
-  HWND hwnd = ::CreateWindowW(wc.lpszClassName, title, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, json["screen_width"].GetInt(), json["screen_height"].GetInt(), nullptr, nullptr, wc.hInstance, nullptr);
-  Deallocate(title, allocator_data);
-  ::ShowWindow(hwnd, SW_SHOWDEFAULT);
-  ::UpdateWindow(hwnd);
-  ::DestroyWindow(hwnd);
-  ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
+  auto json = GetJson("tests/config.json", allocator_data);
+  auto window_info = CreateWin32Window(json, allocator_data);
+  DestroyWin32Window(window_info, allocator_data);
 }
