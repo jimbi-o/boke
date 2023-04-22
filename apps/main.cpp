abf7e5d7a307f54f1d9985add104e6e988a908f1
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2023-04-22 15:00:28 +0900

    adding framework

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,8 +1,5 @@
-#include "boke/util.h"
+#include "boke/framework.h"
 #include "spdlog/spdlog.h"
 int main(int argc, char* argv[]) {
-  using namespace boke;
-  const auto val = Align(3U, 8U);
-  spdlog::info("hello {}", val);
-  return 0;
+  return boke::Run(argv[1]);
 }
---
+++ include/boke/framework.h
@@ -0,0 +1,4 @@
+#pragma once
+namespace boke {
+int32_t Run(const char* const config_path);
+}
