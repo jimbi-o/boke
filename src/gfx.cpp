a7d550d6c4509232904d9d0717460af2d4de9054
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2023-04-23 18:05:54 +0900

    create swapchain

--- src/gfx.cpp
+++ src/gfx.cpp
@@ -20,6 +20,7 @@ extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg
 namespace {
 using namespace boke;
 using D3d12Device = ID3D12Device10;
+using DxgiSwapchain = IDXGISwapChain4;
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
   if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam)) { return true; }
@@ -221,6 +222,120 @@ auto CreateDevice(HMODULE library, IDXGIAdapter4* adapter) {
   device->SetName(L"device");
   return device;
 }
+auto CreateCommandQueue(D3d12Device* const device, const D3D12_COMMAND_LIST_TYPE type, const D3D12_COMMAND_QUEUE_PRIORITY priority, const D3D12_COMMAND_QUEUE_FLAGS flags, const UINT node_mask = 0) {
+  D3D12_COMMAND_QUEUE_DESC desc = { .Type = type, .Priority = priority, .Flags = flags, .NodeMask = node_mask, };
+  ID3D12CommandQueue* command_queue = nullptr;
+  auto hr = device->CreateCommandQueue(&desc, IID_PPV_ARGS(&command_queue));
+  DEBUG_ASSERT(SUCCEEDED(hr), DebugAssert{});
+  return command_queue;
+}
+auto DisableAltEnterFullscreen(HWND hwnd, IDXGIFactory7* factory) {
+  const auto hr = factory->MakeWindowAssociation(hwnd, DXGI_MWA_NO_ALT_ENTER);
+  if (FAILED(hr)) {
+    spdlog::warn("DisableAltEnterFullscreen failed. {}", hr);
+  }
+}
+auto IsVariableRefreshRateSupported(IDXGIFactory7* factory) {
+  BOOL result = false;
+  const auto hr = factory->CheckFeatureSupport(DXGI_FEATURE_PRESENT_ALLOW_TEARING, &result, sizeof(result));
+  if (FAILED(hr)) {
+    spdlog::warn("CheckFeatureSupport(tearing) failed. {}", hr);
+  }
+  return static_cast<bool>(result);
+}
+auto CreateSwapchain(IDXGIFactory7* factory, ID3D12CommandQueue* command_queue, HWND hwnd, const DXGI_FORMAT format, const uint32_t backbuffer_num) {
+  DXGI_SWAP_CHAIN_DESC1 desc = {};
+  desc.Width = desc.Height = 0; // get value from hwnd
+  desc.Format = format;
+  desc.Stereo = false;
+  desc.SampleDesc.Count = 1;
+  desc.SampleDesc.Quality = 0;
+  desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+  desc.BufferCount = backbuffer_num;
+  desc.Scaling = DXGI_SCALING_NONE;
+  desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+  desc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+  desc.Flags = DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT;
+  // desc.Flags = (variable_refresh_rate_supported ? DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING : 0) | DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT;
+  IDXGISwapChain1* swapchain_base = nullptr;
+  auto hr = factory->CreateSwapChainForHwnd(command_queue, hwnd, &desc, nullptr, nullptr, &swapchain_base);
+  DEBUG_ASSERT(SUCCEEDED(hr), DebugAssert{});
+  DxgiSwapchain* swapchain{};
+  hr = swapchain_base->QueryInterface(IID_PPV_ARGS(&swapchain));
+  DEBUG_ASSERT(SUCCEEDED(hr), DebugAssert{});
+  swapchain_base->Release();
+  DEBUG_ASSERT(SUCCEEDED(hr), DebugAssert{});
+  return swapchain;
+#if 0
+  // TODO
+  hr = swapchain_->SetMaximumFrameLatency(frame_buffer_num);
+  DEBUG_ASSERT(SUCCEEDED(hr), DebugAssert{});
+  frame_latency_waitable_object_ = swapchain_->GetFrameLatencyWaitableObject();
+  // get swapchain params
+  {
+    DXGI_SWAP_CHAIN_DESC1 desc = {};
+    auto hr = swapchain_->GetDesc1(&desc);
+    if (FAILED(hr)) {
+      logwarn("swapchain_->GetDesc1 failed. {}", hr);
+    } else {
+      width_ = desc.Width;
+      height_ = desc.Height;
+      format_ = desc.Format;
+      swapchain_buffer_num_ = desc.BufferCount;
+    }
+  }
+  // get swapchain resource buffers for rtv
+  {
+    resources_ = AllocateArraySystem<ID3D12Resource*>(swapchain_buffer_num_);
+    for (uint32_t i = 0; i < swapchain_buffer_num_; i++) {
+      ID3D12Resource* resource = nullptr;
+      auto hr = swapchain_->GetBuffer(i, IID_PPV_ARGS(&resource));
+      if (FAILED(hr)) {
+        spdlog::error("swapchain_->GetBuffer failed. {} {}", i, hr);
+        assert(false && "swapchain_->GetBuffer failed.");
+        for (uint32_t j = 0; j < i; j++) {
+          resources_[i]->Release();
+        }
+        for (uint32_t j = 0; j < swapchain_buffer_num_; j++) {
+          resources_[i] = nullptr;
+        }
+        return false;
+      }
+      resources_[i] = resource;
+      SetD3d12Name(resources_[i], "swapchain" + std::to_string(i));
+    }
+  }
+  // prepare rtv
+  {
+    D3D12_DESCRIPTOR_HEAP_DESC descriptor_heap_desc {
+      .Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV,
+      .NumDescriptors = swapchain_buffer_num_,
+      .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE,
+      .NodeMask = 0
+    };
+    auto hr = device->CreateDescriptorHeap(&descriptor_heap_desc, IID_PPV_ARGS(&descriptor_heap_));
+    if (FAILED(hr)) {
+      spdlog::error("swapchain CreateDescriptorHeap failed. {} {}", hr, swapchain_buffer_num_);
+      assert(false && "swapchain CreateDescriptorHeap failed.");
+      return false;
+    }
+    descriptor_heap_->SetName(L"swapchain descriptor heap");
+    const D3D12_RENDER_TARGET_VIEW_DESC rtv_desc = {
+      .Format = format_,
+      .ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D,
+      .Texture2D = {},
+    };
+    auto rtv_handle = descriptor_heap_->GetCPUDescriptorHandleForHeapStart();
+    auto rtv_step_size = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+    cpu_handles_rtv_ = AllocateArraySystem<D3D12_CPU_DESCRIPTOR_HANDLE>(swapchain_buffer_num_);
+    for (uint32_t i = 0; i < swapchain_buffer_num_; i++) {
+      device->CreateRenderTargetView(resources_[i], &rtv_desc, rtv_handle);
+      cpu_handles_rtv_[i] = rtv_handle;
+      rtv_handle.ptr += rtv_step_size;
+    }
+  }
+#endif
+}
 auto GetDxgiFormat(const char* format) {
   if (strcmp(format, "R8G8B8A8_UNORM") == 0) {
     return DXGI_FORMAT_R8G8B8A8_UNORM;
@@ -311,6 +426,12 @@ TEST_CASE("imgui") {
   auto dxgi = InitDxgi<AdapterType::kHighPerformance>(gfx_libraries.dxgi_library);
   auto device = CreateDevice(gfx_libraries.d3d12_library, dxgi.adapter);
   REQUIRE_NE(device, nullptr);
+  auto command_queue = CreateCommandQueue(device, D3D12_COMMAND_LIST_TYPE_DIRECT, D3D12_COMMAND_QUEUE_PRIORITY_NORMAL, D3D12_COMMAND_QUEUE_FLAG_NONE);
+  REQUIRE_NE(command_queue, nullptr);
+  const auto swapchain_format = GetDxgiFormat(json["swapchain"]["format"].GetString());
+  const auto swapchain_backbuffer_num = json["swapchain"]["num"].GetInt();
+  auto swapchain = CreateSwapchain(dxgi.factory, command_queue, window_info.hwnd, swapchain_format, swapchain_backbuffer_num);
+  REQUIRE_NE(swapchain, nullptr);
   auto descriptor_heap = CreateDescriptorHeap(device, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, json["descriptor_handles"]["shader_visible_buffer_num"].GetUint(), D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE);
   REQUIRE_NE(descriptor_heap, nullptr);
   const auto descriptor_handle_heap_info_full = GetDescriptorHandleHeapInfoFull(descriptor_heap, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, device);
@@ -318,20 +439,33 @@ TEST_CASE("imgui") {
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
   ImGuiIO& io = ImGui::GetIO();
-  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-  io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
   ImGui_ImplWin32_Init(window_info.hwnd);
   ImGui_ImplDX12_Init(device,
-                      json["swapchain"]["num"].GetInt(),
-                      GetDxgiFormat(json["swapchain"]["format"].GetString()),
+                      swapchain_backbuffer_num,
+                      swapchain_format,
                       descriptor_heap,
                       imgui_font_handle_cpu,
                       imgui_font_handle_gpu);
   const uint32_t max_loop_num = json["max_loop_num"].GetUint();
   for (uint32_t frame_count = 0; frame_count < max_loop_num; frame_count++) {
     if (ProcessWindowMessages() == WindowMessage::kQuit) { break; }
+#if 0
+    ImGui_ImplDX12_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+    ImGui::NewFrame();
+    ImGui::ShowDemoWindow();
+    ImGui::Render();
+    ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), command_list);
+#endif
   }
+  ImGui_ImplDX12_Shutdown();
+  ImGui_ImplWin32_Shutdown();
+  ImGui::DestroyContext();
   descriptor_heap->Release();
+  swapchain->Release();
+  command_queue->Release();
   device->Release();
   TermDxgi(dxgi);
   ReleaseGfxLibraries(gfx_libraries);
