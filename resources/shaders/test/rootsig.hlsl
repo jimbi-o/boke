a199054edc0befaaa5c755e3ee4beee461175d8f
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2023-05-26 14:09:50 +0900

    changed sampler to static sampler

--- resources/shaders/test/rootsig.hlsl
+++ resources/shaders/test/rootsig.hlsl
@@ -1,4 +1,4 @@
 #define ROOTSIG_EMPTY "RootFlags(0)"
 #define ROOTSIG_SRV "RootFlags(DENY_VERTEX_SHADER_ROOT_ACCESS),"  \
   "DescriptorTable(SRV(t0, numDescriptors = unbounded, flags = DESCRIPTORS_VOLATILE | DATA_STATIC_WHILE_SET_AT_EXECUTE))," \
-  "DescriptorTable(Sampler(s0, numDescriptors = unbounded, flags = DESCRIPTORS_VOLATILE))"
+  "StaticSampler(s0, filter=FILTER_MIN_MAG_MIP_LINEAR, addressU=TEXTURE_ADDRESS_CLAMP, addressV=TEXTURE_ADDRESS_CLAMP,visibility=SHADER_VISIBILITY_PIXEL)"
