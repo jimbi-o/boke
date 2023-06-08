f625da099fbee97bb518b1024b97ed6406714f2e
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2023-06-08 11:00:41 +0900

    postprocess ms bug fixed

--- resources/shaders/test/postprocess_ms.hlsl
+++ resources/shaders/test/postprocess_ms.hlsl
@@ -8,11 +8,11 @@ struct VertexOut {
 [OutputTopology("triangle")]
 void main(out vertices VertexOut vertices[VERTEX_NUM], out indices uint3 faces[FACE_NUM]) {
   SetMeshOutputCounts(VERTEX_NUM, FACE_NUM);
-  vertices[0].position = float4(-1.0f, -1.0f, 0.0f, 1.0f);
+  vertices[0].position = float4(-1.0f,  1.0f, 0.0f, 1.0f);
   vertices[0].uv = float2(0.0f, 0.0f);
-  vertices[1].position = float4(-1.0f,  3.0f, 0.0f, 1.0f);
-  vertices[1].uv = float2(0.0f, 2.0f);
-  vertices[2].position = float4( 3.0f,  3.0f, 0.0f, 1.0f);
-  vertices[2].uv = float2(2.0f, 0.0f);
+  vertices[1].position = float4( 3.0f,  1.0f, 0.0f, 1.0f);
+  vertices[1].uv = float2(2.0f, 0.0f);
+  vertices[2].position = float4(-1.0f, -3.0f, 0.0f, 1.0f);
+  vertices[2].uv = float2(0.0f, 2.0f);
   faces[0] = uint3(0, 1, 2);
 }
